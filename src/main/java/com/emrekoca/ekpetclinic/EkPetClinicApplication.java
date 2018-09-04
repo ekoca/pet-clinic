1cd4a0a40bfdf5797eea52a59e856d0181cea1e1
Emre <emre@EKs-MacBook-Pro.local>
2018-09-03 17:06:12 -0700

    Initial commit

---
+++ src/main/java/com/emrekoca/ekpetclinic/EkPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.emrekoca.ekpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/emrekoca/ekpetclinic/EkPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emrekoca.ekpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EkPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
