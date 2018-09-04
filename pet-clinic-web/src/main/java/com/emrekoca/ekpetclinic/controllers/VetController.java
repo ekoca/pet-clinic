d8284d76ccc1425ca6e658477ec0d2a226313919
Emre Koca <ekocausa@gmail.com>
2018-09-03 22:48:07 -0700

    Listed all the vets. Closes #15.

--- pet-clinic-web/src/main/java/com/emrekoca/ekpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/emrekoca/ekpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.emrekoca.ekpetclinic.controllers;
 
+import com.emrekoca.ekpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    private final VetService vetService;
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String vets(){
+    public String vets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
