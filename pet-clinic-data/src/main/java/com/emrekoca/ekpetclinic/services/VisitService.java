f1ea5d22dd4f3d940717fb886120a6f2f4cf1dd6
Emre Koca <ekocausa@gmail.com>
2018-09-07 22:20:53 -0700

    Added visit map service.

---
+++ pet-clinic-data/src/main/java/com/emrekoca/ekpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.emrekoca.ekpetclinic.services;
+
+import com.emrekoca.ekpetclinic.model.visit.Visit;
+
+/**
+ * Created by Emre.
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/emrekoca/ekpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.emrekoca.ekpetclinic.services.map;
+
+import com.emrekoca.ekpetclinic.model.visit.Visit;
+import com.emrekoca.ekpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Created by Emre.
+ */
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Optional<Visit> findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Optional<Visit> save(Visit visit) {
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+}
